7ecb481a3a2d15e074036abdd84eb843ddac5e58
Reynaldo <reyyy0207@gmail.com>
2025-12-29 14:44:24 +0700

    Update Version

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { PRODUCTS_DATA } from "@/app/components/constants/products";
 import ViewDetail from "@/app/components/views/detail";
+import { redirect } from "next/navigation";
 
 async function Page({ props }: { props: { params: Promise<{ id: string }> } }) {
   const { id } = await props.params;
   const product = PRODUCTS_DATA.find((p) => p.id === parseInt(id));
 
-  if (!product) return null;
+  if (!product) return redirect("/");
 
   return <ViewDetail product={product} />;
 }
